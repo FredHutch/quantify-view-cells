3b32019f74b8675333a6fc6545971e1e74c22f4d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-01 03:58:18 +0000

    Adding tarfile member sanitization to extractall()

--- visualize.py
+++ visualize.py
@@ -70,7 +70,26 @@ def read_images(fp):
         os.mkdir(folder)
         logger.info(f"Decompressing {fp} into {folder}")
         with tarfile.open(fp, "r:gz") as tar:
-            tar.extractall(folder)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, folder)
 
     # Keep track of the path for each image, keyed by image and parameters
     images = defaultdict(dict)
